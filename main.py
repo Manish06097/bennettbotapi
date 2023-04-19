2145dcfebf8a32c4a065aa33696ae68d0b52032e
Manish Agarwal <73208573+Manish06097@users.noreply.github.com>
2023-04-19 20:11:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from typing import Optional
-
+# main.py
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from langchain.vectorstores import FAISS
+from langchain import OpenAI
+from langchain.chains import RetrievalQA 
+from langchain.embeddings import HuggingFaceEmbeddings
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+name = FAISS.load_local(embeddings=HuggingFaceEmbeddings(),folder_path='')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Message(BaseModel):
+    message: str
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/chat")
+async def chat_endpoint(message: Message):
+    user_message = message.message
+    reply = process_message(user_message)
+    return {"reply": reply}
+
+def process_message(user_message: str) -> str:
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(model='text-davinci-003',openai_api_key="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"), chain_type='stuff', retriever=name.as_retriever())
+    ans = qa.run(user_message)
+    return f"Bot {ans}"
